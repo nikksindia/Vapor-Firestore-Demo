be12c18dc9c642c5144a14cffcae54ef5e2644a5
Nikhil Sharma <sci.nikks@gmail.com>
2018-09-18 00:18:46 +0530

    default port changes to 8081

---
+++ VaporFirestoreDemo/Sources/App/Controllers/NextRandomController.swift
@@ -0,0 +1,8 @@
+//
+//  NextRandomController.swift
+//  App
+//
+//  Created by Nikhil Sharma on 17/09/18.
+//
+
+import Foundation
--- VaporFirestoreDemo/Sources/App/configure.swift
+++ VaporFirestoreDemo/Sources/App/configure.swift
@@ -10,6 +10,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
+    
+    ///change default port to 8081
+    let myService = NIOServerConfig.default(port: 8001)
+    services.register(myService)
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
